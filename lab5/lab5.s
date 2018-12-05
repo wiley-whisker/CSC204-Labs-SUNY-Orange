f7eb2ad08bcde2ec7e6c556cd4f89e57dddd363f
John Doe <wsmatthews99@gmail.com>
2018-12-04 22:43:02 -0500

    lab5 almost done

--- lab5/lab5.s
+++ lab5/lab5.s
@@ -2,7 +2,9 @@
 
 .text
 main:
-    pushq %rbp
+    pushq %rbp # to fix misalignment
+
+    # GET USER INPUT: WHILE LOOP while(rcx < 10) 
     xor %rax, %rax # clear rax
     movq $x, %rbx # load location of array into rbx
     movq $0, %rcx # load the number of inputs into rcx (i)
@@ -18,10 +20,8 @@ main:
         call printf
 
         # popem in order
-        popq %rcx # get array ptr back from stack
-        popq %rbx # get i back from stack
-
-        lea (%rbx, %rcx, 8), %rbx # Load x[rcx] into rdx.
+        popq %rcx # get i back from stack
+        popq %rbx # get array ptr back from stack# get i back from stack
 
         # save registers
         pushq %rbx
@@ -29,19 +29,59 @@ main:
 
         xorq %rax, %rax # no args on stack
         movq $percentd, %rdi # load percentd as 1st arg
-        movq $temp, %rsi # load current array index as 2nd arg
+        movq %rbx, %rsi # load current array index ptr as 2nd arg
         call scanf
  
         # popem in order
         popq %rcx # get array ptr back from stack
         popq %rbx # get i back from stack
-        movq temp,%rdx
-        movq %rdx, (%rbx)
+
+        addq $8, %rbx
+
+        # Increment rcx(i) and repeat loop if rcx < 10
+        inc %rcx
         cmpq $10, %rcx # check while condition
         jl in1 
     endin1:
 
+    # SUM THE NUMBERS
+    xorq %rax, %rax # clear rax
+    movq $x, %rbx
+    movq $0, %rcx # load the number of array indecies into rcx (i)
+    cmpq $10, %rcx # compare i vs iteration #
+    jge endsum
+    sumloop:
+        add (%rbx), %rax # Add x[rcx] to sum(rax)
+
+        addq $8, %rbx
+
+        inc %rcx # rcx++.
+
+        # Compare for repeat.
+        cmpq $10, %rcx # Compare 10(len of array) against i (rcx).
+        jl sumloop # Repeat loop while rcx < 10
+    endsum:
+
+    movq %rax, sum # Move sum into variable
+
+    # Save registers
+    pushq %rcx
+    pushq %rbx
+
+    # Print sum
+    xor %rax, %rax
+    movq $resultmsg, %rdi
+    movq sum, %rsi
+    call printf
+
+    # Restore registers
+    popq %rbx
+    popq %rcx
+
+
+
     # Exit program.
+    popq %rbp
     movq   $60,    %rax     # exit(
     movq   $0,     %rdi     # EXIT_SUCCESS
     syscall
@@ -52,4 +92,5 @@ main:
     sum: .quad 0
     prompt: .asciz "Please enter a number: \n"
     percentd: .asciz "%i \n"
-    temp: .quad 0
+    resultmsg: .asciz "The sum is: %d\n"
+    dbmsg: .asciz "This-> %d\n"
